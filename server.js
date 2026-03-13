c8c963057164cace16b597bbafa4a0dd97073d55
Akshvt <128501282+Akshvt@users.noreply.github.com>
2026-03-13 14:00:21 +0530

    Implemented mail and Ai

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ import connectDB from "./src/config/database.js";
 
 const PORT = process.env.PORT || 8000;
 
-connectDB()
-    .catch((err) => {
-        console.error("MongoDB connection failed:", err);
-        process.exit(1);
-    });
+// connectDB()
+//     .catch((err) => {
+//         console.error("MongoDB connection failed:", err);
+//         process.exit(1);
+//     });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
