ae2720541230bbe7e7d953ebe4e4c9802e88fbd2
Akshvt <128501282+Akshvt@users.noreply.github.com>
2026-04-03 22:38:57 +0530

    Add socket.io integration for real-time communication

---
+++ Backend/src/sockets/server.sockets.js
@@ -0,0 +1,18 @@
+import { Server } from "socket.io"
+
+let io;
+
+
+export function initSocket(httpServer){
+    io = new Server(httpServer, {
+        cors: {
+            origin: "http://localhost:5173",
+            credentials: true,
+        }
+    })
+
+
+    io.on("connection", (socket)=>{
+        console.log("A user connected: " +socket.id) //unique user id that changes everytime a new connection is created
+    })
+}
